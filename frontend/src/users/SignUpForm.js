da389e07ac257783cc0fc8a78d9460d55f727b1c
Yee Ching Hui <eugenehui93@Yees-MBP.lan>
2023-04-24 20:46:52 -0700

    added login and sign up form

--- frontend/src/users/SignUpForm.js
+++ frontend/src/users/SignUpForm.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect, useReducer } from "react"
 import { useHistory, useParams } from "react-router"
 
 function SignUpForm() {
@@ -66,6 +66,18 @@ function SignUpForm() {
 							id="email"
 							name="email"
 						/>
+					</div>
+					<div className="col-sm-6 form-group">
+						<label htmlFor="password">Password</label>
+						<input
+							type="password"
+							required
+							value={user.password}
+							onChange={e=> setUser({...user, password: e.target.value})}
+							className="form-control"
+							id="password"
+							name="password"
+							/>
 					</div>
 				</div>
 				<input className="btn btn-primary" type="submit" value="Sign Up" />

cbfe17fa6541b6866de0d863c95792be50ccf71a
Yee Ching Hui <eugenehui93@Yees-MBP.lan>
2023-04-26 19:52:57 -0700

    required JWT

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -16,12 +16,33 @@ router.post('/', async (req, res) => {
 
     }
     else{
-        res.json({user})
+        const result = await jwt.encode(processs.env.JWT_SECRET, {id:user.userId})
+        res.json({user:user,token:result.value})
     }
 })
 
 module.exports = router
 
 router.get('/profile', async (req, res) => {
-   
-})
+    try {
+            const [method, token] = req.headers.authorization.split(' ')
+
+            if (method == 'Bearer') {
+
+            const result = await jwt.decode(process.env.JWT_SECRET, token)
+
+            const { id } = result.value
+
+            let user = await User.findOne({
+                where: {
+                        userId: id
+                    }
+                })           
+                res.json(user)
+            }
+        } catch (err){ 
+                res.json(null)
+            }
+        })
+        
+        module.exports = router
